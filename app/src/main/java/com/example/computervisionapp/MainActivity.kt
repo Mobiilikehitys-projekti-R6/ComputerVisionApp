28e04f7bfba18f7166ce773c6754f466dcd375c4
NikkeDoy <niklas.siltala@gmail.com>
2023-03-29 12:52:11 +0300

    ✨ | Added functionality to select image from gallery.

--- app/src/main/java/com/example/computervisionapp/MainActivity.kt
+++ app/src/main/java/com/example/computervisionapp/MainActivity.kt
@@ -1,25 +1,40 @@
 package com.example.computervisionapp
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.label.ImageLabeling
 import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 import java.io.IOException
 
+
 class MainActivity : AppCompatActivity() {
+    var pickedPhoto: Uri? = null
+    var pickedBitmap: Bitmap? = null
+    var img: ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val img: ImageView = findViewById(R.id.imageToLabel)
         val txtOutput : TextView = findViewById(R.id.txtOutput)
+        img = findViewById(R.id.imageToLabel)
         val btn: Button = findViewById(R.id.btnTest)
+        val select: Button = findViewById(R.id.btnSelect)
 
         // get a list of all file names in the assets folder
         val fileNames = assets.list("")
@@ -30,12 +45,16 @@ class MainActivity : AppCompatActivity() {
         // load the bitmap from the first image file
         val bitmap: Bitmap? = fileName?.let { assetsToBitmap(it) }
         bitmap?.apply {
-            img.setImageBitmap(this)
+            img?.setImageBitmap(bitmap)
+        }
+
+        select.setOnClickListener {
+            pickPhoto(view = select.rootView)
         }
 
         btn.setOnClickListener {
             val labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
-            val image = InputImage.fromBitmap(bitmap!!, 0)
+            val image = InputImage.fromBitmap(pickedBitmap!!, 0)
             var outputText = ""
             labeler.process(image)
                 .addOnSuccessListener { labels ->
@@ -51,6 +70,48 @@ class MainActivity : AppCompatActivity() {
                     // Task failed with an exception
                 }
         }
+    }
+
+    fun pickPhoto(view: View) {
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        } else {
+            val galleryIntext = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(galleryIntext, 2)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == 1) {
+            if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val galleryIntext = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galleryIntext, 2)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == 2 && resultCode == Activity.RESULT_OK && data != null) {
+            pickedPhoto = data.data
+            if(pickedPhoto != null) {
+                if(Build.VERSION.SDK_INT >= 28) {
+                    val source = ImageDecoder.createSource(this.contentResolver, pickedPhoto!!)
+                    pickedBitmap = ImageDecoder.decodeBitmap(source)
+                }
+                else {
+                    pickedBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, pickedPhoto)
+                }
+                img?.setImageBitmap(pickedBitmap)
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
 // extension function to get bitmap from assets
